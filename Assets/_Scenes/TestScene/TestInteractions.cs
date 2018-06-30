05b7aa9c41a8a9e9a411c8c482486b92497ed9da
WangASong-1 <1485728127@qq.com>
2018-06-30 09:41:23 +0800

    测试。Final IK学习

---
+++ Assets/_Scenes/TestScene/TestInteractions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.FinalIK;
+
+public class TestInteractions : MonoBehaviour {
+    public FullBodyBipedIK fullBodyIK;
+    public Transform rightHand, leftHand;
+
+    private void LateUpdate()
+    {
+        fullBodyIK.solver.rightHandEffector.position = rightHand.position;
+        fullBodyIK.solver.rightHandEffector.rotation = rightHand.rotation;
+        fullBodyIK.solver.rightHandEffector.positionWeight = 1;
+        fullBodyIK.solver.rightHandEffector.rotationWeight = 1;
+
+        fullBodyIK.solver.leftHandEffector.position = leftHand.position;
+        fullBodyIK.solver.leftHandEffector.rotation = leftHand.rotation;
+        fullBodyIK.solver.leftHandEffector.positionWeight = 1;
+        fullBodyIK.solver.leftHandEffector.rotationWeight = 1;
+    }
+}
---
+++ Assets/_Scenes/TestScene/aSong_GraMP_40.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.FinalIK;
+
+public class aSong_GraMP_40 : MonoBehaviour {
+    [Tooltip("The object to interact to")]
+    [SerializeField] InteractionObject interactionObject;
+    [Tooltip("The effectors to interact with")]
+    [SerializeField] FullBodyBipedEffector[] effectors;
+
+    private InteractionSystem interactionSystem;
+    private Animator mAnim;
+
+    public float magnitude = 1f;
+    private Recoil recoil;
+
+    void Awake()
+    {
+        interactionSystem = GetComponent<InteractionSystem>();
+        mAnim = GetComponent<Animator>();
+
+        recoil = GetComponent<Recoil>();
+    }
+
+    void GrabMP_40()
+    {
+        //mAnim.SetLayerWeight(1, 1);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E键按下");
+            foreach (FullBodyBipedEffector e in effectors)
+            {
+                interactionSystem.StartInteraction(e, interactionObject, true);
+
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("R键按下");
+            recoil.Fire(magnitude);
+        }
+    }
+}
