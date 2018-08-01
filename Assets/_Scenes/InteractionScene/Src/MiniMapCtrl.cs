96f71f7613b1d902d23f371f789782c8f5c3a57b
WangAsong <wxs3314480@sina.cn>
2018-08-01 11:34:33 +0800

    增加小地图功能

---
+++ Assets/_Scenes/InteractionScene/Src/MiniMapCtrl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Utility;
+
+public class MiniMapCtrl : MonoBehaviour {
+    public FollowTarget mMapFollow;
+    public Slider mScaleSlider;
+    public Transform mMap;
+
+    public bool b_mapClicked = false;
+
+    private Vector3 preMousePos = Vector3.zero;
+    private bool b_mapBeDragged = false;
+
+    private void Awake()
+    {
+        mScaleSlider.value = mMapFollow.offset.y;
+    }
+
+    public void ScaleMap()
+    {
+        //map.localScale = Vector3.one * mScaleSlider.value;
+        mMapFollow.offset.y = mScaleSlider.value;
+    }
+
+    public void ClickMap()
+    {
+        if (b_mapBeDragged)
+        {
+            b_mapBeDragged = false;
+            preMousePos = Vector3.zero;
+            return;
+        }
+        b_mapClicked = !b_mapClicked;
+        if (b_mapClicked)
+        {
+            transform.localScale = Vector3.one * 2f;
+            mMapFollow.offset.y = mScaleSlider.minValue;
+        }
+        else
+        {
+            transform.localScale = Vector3.one ;
+            mMapFollow.offset.y = mScaleSlider.maxValue;
+        }
+        mScaleSlider.value = mMapFollow.offset.y;
+    }
+
+    public void DragMap()
+    {
+        if (!b_mapBeDragged)
+        {
+            b_mapBeDragged = true;
+            preMousePos = Input.mousePosition;
+        }
+        mMap.position += Input.mousePosition - preMousePos;
+        preMousePos = Input.mousePosition;
+
+        Vector3 vec = mMap.localPosition;
+        vec.x = Mathf.Abs(vec.x) > 50f ? 50f * Mathf.Sign(vec.x) : vec.x;
+        vec.y = Mathf.Abs(vec.y) > 50f ? 50f * Mathf.Sign(vec.y) : vec.y;
+        mMap.localPosition = vec;
+        //vec.x = Mathf.Abs(vec.x) > 50f ? 50f * Mathf.Sign(vec.x) : vec.x;
+    }
+}
