409bac7bdf99f23d854d9bf15d0d3a60166efb1b
WangAsong <wxs3314480@sina.cn>
2018-09-03 09:50:19 +0800

    想要的消息系统加入

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,24 +1,115 @@
 ﻿using UnityEngine;
+using UnityEngine.Profiling;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using System;
 
 public class test : MonoBehaviour {
 
     public Transform A, B;
 
+    private Dictionary<int, GameObject> mDic = new Dictionary<int, GameObject>();
+    private Dictionary<int, int> mDic_int = new Dictionary<int, int>();
+
+    private Hashtable mHash = new Hashtable();
+
+
     void Start()
     {
-        //设置A的欧拉角
-        //试着更改各个分量查看B的不同旋转状态
-        //A.eulerAngles = new Vector3(1.0f, 1.5f, 2.0f);
-        //Debug.Log("111111111111111111111111111111111111111" + B.rotation * B.position);
-
-        aSong_UnityJsonUtil.Init("","bbb");
-        //
-        //aSong_UnityJsonUtil.SetString("123", "456");
-        //aSong_UnityJsonUtil.Save(true);
-        aSong_UnityJsonUtil.Read();
-
-        //相当于将该物体放到偏移该物体指定该位移值的位置(坐标系是以该物体)
         transform.position = transform.rotation * new Vector3(0.02f, -0.6f, 0);
+
+        mDic.Add(1, gameObject);
+        mDic.Add(2, gameObject);
+        mDic.Add(3, gameObject);
+        mDic.Add(4, gameObject);
+
+
+        mDic_int.Add(1, 1);
+        mDic_int.Add(2, 2);
+        mDic_int.Add(3, 3);
+        mDic_int.Add(4, 4);
+        mDic_int.Add(5, 5);
+
+        mHash.Add(1, 1);
+    }
+
+    void DelegateAAA()
+    {
+        Profiler.BeginSample("委托 action");
+
+        Add(1, 2, delegate {
+            //Debug.Log("匿名delegate");
+        });
+        Profiler.EndSample();
+        Profiler.BeginSample("委托 func");
+        int abc = 2;
+        AddBBB(1, 2, delegate (string a) {
+            //Debug.Log("匿名delegate" + a);
+            return int.Parse( a) + abc;
+        });
+        Profiler.EndSample();
+    }
+
+    void Add(int a, int b, UnityAction action)
+    {
+        //Debug.Log("a+b = " +(a+b).ToString());
+        action();
+    }
+
+    void AddBBB(int a, int b, Func<string, int> action)
+    {
+        //Debug.Log("a+b = " + (a + b).ToString());
+        Debug.Log( action((a + b).ToString()));
+    }
+
+    private void Update()
+    {
+        DelegateAAA();
+
+        UnityEngine.Object o;
+
+        Profiler.BeginSample("字典foreach");
+        foreach(var a in mDic)
+        {
+            //Debug.Log("1222" + 1);
+            o = a.Value;
+        }
+        Profiler.EndSample();
+        Profiler.BeginSample("字典foreach int");
+        foreach (var a in mDic_int)
+        {
+            //Debug.Log("1222" + 1);
+        }
+        Profiler.EndSample();
+        Profiler.BeginSample("字典while 迭代器 var");
+        var enumerator = mDic_int.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+
+        }
+        Profiler.EndSample();
+
+        Profiler.BeginSample("字典while 迭代器 ");
+
+        Dictionary<int, int>.Enumerator IDictionaryEnumerator = mDic_int.GetEnumerator();
+        int aaa = 0;
+        string bbb;//= "123" + 22.ToString();
+        while (IDictionaryEnumerator.MoveNext())
+        {
+            //Debug.Log("123");
+            aaa = IDictionaryEnumerator.Current.Value;
+        }
+        Profiler.EndSample();
+
+        Profiler.BeginSample("哈希 迭代器 ");
+
+        var abc = mHash.GetEnumerator();
+        while (abc.MoveNext())
+        {
+            //Debug.Log("123");
+            aaa = (int)abc.Value;
+        }
+        Profiler.EndSample();
     }
 }
